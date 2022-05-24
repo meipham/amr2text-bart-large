10c7588257e9634d34e4b7cc3d4c5487920b8fe4
meipham <phamuyen.cse@gmail.com>
2022-05-24 15:53:04 +0700

    Update data.py

--- fine-tune/data_interface/data.py
+++ fine-tune/data_interface/data.py
@@ -1,22 +1,3 @@
-# coding=utf-8
-# Copyright 2020 The TensorFlow Datasets Authors and the HuggingFace Datasets Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# Lint as: python3
-"""AMR dataset."""
-
-
 from inspect import EndOfBlock
 import json
 import os
@@ -24,7 +5,6 @@ import os
 import datasets
 
 logger = datasets.logging.get_logger(__name__)
-
 
 _DESCRIPTION = """
 
@@ -67,13 +47,14 @@ class AMRData(datasets.GeneratorBasedBuilder):
             datasets.SplitGenerator(name=datasets.Split.TEST, gen_kwargs={"filepath": test_path}),
         ]
 
-    def _generate_examples(self, filepath):
+    def _generate_examples(self, filepaths):
         """Yields examples."""
-        logger.info("generating examples from = %s", filepath)
-        with open(filepath, "r", encoding="utf-8") as f:
-            lines = f.readlines()
-            for idx, line in enumerate(lines):
-                json_dict = json.loads(line.strip())
-                src = json_dict["src"]
-                tgt = json_dict["tgt"]
-                yield idx, {_SRC: src, _TGT: tgt}
+        logger.info("generating examples from = %s", filepaths)
+        for filepath in filepaths:
+            with open(filepath, "r", encoding="utf-8") as f:
+                lines = f.readlines()
+                for idx, line in enumerate(lines):
+                    json_dict = json.loads(line.strip())
+                    src = json_dict["src"]
+                    tgt = json_dict["tgt"]
+                    yield idx, {_SRC: src, _TGT: tgt}
